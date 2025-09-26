706668922f207f39a473b23d50ab264959f0dd40
Dennis Wang <66754085+denniwang@users.noreply.github.com>
2025-09-26 15:08:25 -0400

    added infisical as a provider in aws

--- infrastructure/aws/providers.tf
+++ infrastructure/aws/providers.tf
@@ -4,9 +4,22 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.14.1"
     }
+    infisical = {
+      source = "infisical/infisical"
+    }
   }
 }
 
 provider "aws" {
   region = "us-east-2"
+}
+
+provider "infisical" {
+  host = "https://app.infisical.com"
+  auth = {
+    universal = {
+      client_id     = var.infisical_client_id
+      client_secret = var.infisical_client_secret
+    }
+  }
 }
