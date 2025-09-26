1bd467c5ac1751252b6c4f9352e0908eca9bdf02
Dennis Wang <66754085+denniwang@users.noreply.github.com>
2025-09-26 14:18:12 -0400

    added secrets file

--- infrastructure/github/secrets.tf
+++ infrastructure/github/secrets.tf
@@ -55,11 +55,3 @@ variable "infisical_workspace_id" {
   type    = string
   default = "d1ee8b80-118c-4daf-ae84-31da43261b76"
 }
-
-variable "rds_username" {
-  type = string
-}
-
-variable "rds_password" {
-  type = string
-}
