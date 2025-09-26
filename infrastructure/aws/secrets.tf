05a14ccdc4679359f930950f458c8229ae0b7b27
Dennis Wang <66754085+denniwang@users.noreply.github.com>
2025-09-26 15:12:42 -0400

    added missing vars to aws folder

---
+++ infrastructure/aws/secrets.tf
@@ -0,0 +1,31 @@
+data "infisical_secrets" "github_folder" {
+  env_slug     = "dev"
+  workspace_id = var.infisical_workspace_id
+  folder_path  = "/github"
+}
+data "infisical_secrets" "infisical_folder" {
+  env_slug     = "dev"
+  workspace_id = var.infisical_workspace_id
+  folder_path  = "/infisical"
+}
+resource "github_actions_secret" "infisical_client_id" {
+  repository      = github_repository.branch.name
+  secret_name     = "INFISICAL_CLIENT_ID"
+  plaintext_value = data.infisical_secrets.infisical_folder.secrets["infisical-tf-client-id"].value
+}
+
+resource "github_actions_secret" "infisical_client_secret" {
+  repository      = github_repository.branch.name
+  secret_name     = "INFISICAL_CLIENT_SECRET"
+  plaintext_value = data.infisical_secrets.infisical_folder.secrets["infisical-tf-client-secret"].value
+}
+
+
+
+variable "infisical_client_id" {
+  type = string
+}
+
+variable "infisical_client_secret" {
+  type = string
+}
