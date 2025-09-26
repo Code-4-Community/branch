cf450ce5b2d0cd6ae4eabbd2eda63018a7723a48
Dennis Wang <66754085+denniwang@users.noreply.github.com>
2025-09-26 12:11:44 -0400

    test

--- infrastructure/test/backend.tf
+++ infrastructure/test/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "c4c-neu-terraform-state-files"
-    key            = "aws/terraform.tfstate"
+    key            = "test/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
