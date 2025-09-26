3e60532042c730fe5fb17a19c5418a7631885b3c
Dennis Wang <66754085+denniwang@users.noreply.github.com>
2025-09-26 14:29:48 -0400

    waht is going onnn

--- infrastructure/aws/secrets.tf
+++ infrastructure/aws/secrets.tf
@@ -1,3 +1,30 @@
+data "infisical_secrets" "aws_folder" {
+  env_slug     = "dev"
+  workspace_id = var.infisical_workspace_id
+  folder_path  = "/aws"
+}
+
+data "github_repository" "branch" {
+  name = "branch"
+}
+
+resource "github_actions_secret" "rds_username" {
+  repository      = data.github_repository.branch.name
+  secret_name     = "RDS_USERNAME"
+  plaintext_value = data.infisical_secrets.aws_folder.secrets["rds_username"].value
+}
+
+resource "github_actions_secret" "rds_password" {
+  repository      = data.github_repository.branch.name
+  secret_name     = "RDS_PASSWORD"
+  plaintext_value = data.infisical_secrets.aws_folder.secrets["rds_password"].value
+}
+
+variable "infisical_workspace_id" {
+  type    = string
+  default = "d1ee8b80-118c-4daf-ae84-31da43261b76"
+}
+
 variable "rds_username" {
   type = string
 }
--- infrastructure/github/secrets.tf
+++ infrastructure/github/secrets.tf
@@ -4,11 +4,6 @@ data "infisical_secrets" "github_folder" {
   folder_path  = "/github"
 }
 
-data "infisical_secrets" "aws_folder" {
-  env_slug     = "dev"
-  workspace_id = var.infisical_workspace_id
-  folder_path  = "/aws"
-}
 
 data "infisical_secrets" "infisical_folder" {
   env_slug     = "dev"
@@ -40,8 +35,6 @@ resource "github_actions_secret" "infisical_client_secret" {
   secret_name     = "INFISICAL_CLIENT_SECRET"
   plaintext_value = data.infisical_secrets.infisical_folder.secrets["infisical-tf-client-secret"].value
 }
-
-
 
 variable "infisical_client_id" {
   type = string
