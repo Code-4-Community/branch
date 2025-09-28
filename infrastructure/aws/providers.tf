447c866f4adf1da391acc155b648deb9f4022109
Dennis Wang <66754085+denniwang@users.noreply.github.com>
2025-09-28 14:04:12 -0400

    x

--- infrastructure/aws/providers.tf
+++ infrastructure/aws/providers.tf
@@ -18,10 +18,6 @@ provider "aws" {
   region = "us-east-2"
 }
 
-provider "github" {
-  owner = "Code-4-Community"
-  token = data.infisical_secrets.github_folder.secrets["branch-gh-admin"].value
-}
 
 provider "infisical" {
   host = "https://app.infisical.com"
--- infrastructure/aws/secrets.tf
+++ infrastructure/aws/secrets.tf
@@ -3,10 +3,17 @@ data "infisical_secrets" "github_folder" {
   workspace_id = var.infisical_workspace_id
   folder_path  = "/github"
 }
+
 data "infisical_secrets" "infisical_folder" {
   env_slug     = "dev"
   workspace_id = var.infisical_workspace_id
   folder_path  = "/infisical"
+}
+
+data "infisical_secrets" "aws_folder" {
+  env_slug     = "dev"
+  workspace_id = var.infisical_workspace_id
+  folder_path  = "/aws/rds"
 }
 
 data "github_repository" "branch" {
@@ -24,10 +31,20 @@ resource "github_actions_secret" "infisical_client_secret" {
   plaintext_value = data.infisical_secrets.infisical_folder.secrets["infisical-tf-client-secret"].value
 }
 
-variable "infisical_client_id" {
+
+resource "infisical_client_secret" "rds_username" {
+  secret_name     = "RDS_USERNAME"
+  plaintext_value = data.infisical_secrets.infisical_folder.secrets["rds-username"].value
+}
+resource "infisical_client_secret" "rds_password" {
+  secret_name     = "RDS_PASSWORD"
+  plaintext_value = data.infisical_secrets.infisical_folder.secrets["rds-password"].value
+}
+
+variable "rds_username" {
   type = string
 }
 
-variable "infisical_client_secret" {
+variable "rds_password" {
   type = string
 }
