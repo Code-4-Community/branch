8e8d54c2f9836d6462f2998320dc34c8e7864595
Dennis Wang <66754085+denniwang@users.noreply.github.com>
2025-10-09 13:29:23 -0400

    using psql17.6 instead of 18

--- infrastructure/aws/main.tf
+++ infrastructure/aws/main.tf
@@ -2,10 +2,10 @@ resource "aws_db_instance" "branch_rds" {
   allocated_storage    = 10
   db_name              = "branch_rds"
   engine               = "postgres"
-  engine_version       = "18.0"
+  engine_version       = "17.6"
   instance_class       = "db.t3.micro"
   username             = data.infisical_secrets.rds_folder.secrets["username"].value
   password             = data.infisical_secrets.rds_folder.secrets["password"].value
-  parameter_group_name = "default.postgres18.0"
+  parameter_group_name = "default.postgres17.6"
   skip_final_snapshot  = true
 }
