11ca8e6cdc6eb0b8071d8c881f0a7c3f1d5b9d08
Dennis Wang <66754085+denniwang@users.noreply.github.com>
2025-10-09 10:11:10 -0700

    #5 Allocating RDS Instance in Terraform (#18)
    - created aws module
    - allocated rds instance with infisical secrets

---
+++ infrastructure/aws/secrets.tf
@@ -0,0 +1,13 @@
+variable "infisical_client_id" {
+  type = string
+}
+
+variable "infisical_client_secret" {
+  type = string
+}
+
+data "infisical_secrets" "rds_folder" {
+  env_slug     = "dev"
+  workspace_id = var.infisical_workspace_id
+  folder_path  = "/aws/rds"
+}
