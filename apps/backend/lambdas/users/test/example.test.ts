33c7036b87fa343e78f539376a313d94c49d4c1b
Nour Shoreibah <168875317+nourshoreibah@users.noreply.github.com>
2025-10-25 17:49:10 -0400

    Revert "Patch user (#67)"
    This reverts commit d14830733c0ebae9ae08db5635b1928ef17c842b.

--- apps/backend/lambdas/users/test/example.test.ts
+++ apps/backend/lambdas/users/test/example.test.ts
@@ -3,32 +3,3 @@ test("health test 🌞", async () => {
   let res = await fetch("http://localhost:3000/users/health")
   expect(res.status).toBe(200);
 });
-
-test("patch user test 🌞", async () => {
-  let res = await fetch("http://localhost:3000/users/1", {
-    method: "PATCH",
-    body: JSON.stringify({
-      name: "John Branch",
-      email: "mrbranch@example.com",
-      isAdmin: false
-    })
-  })
-  expect(res.status).toBe(200);
-  let body = await res.json().then(r => r.body);
-  console.log(body);
-  expect(body.email).toBe("mrbranch@example.com");
-  expect(body.name).toBe("John Branch");
-  expect(body.isAdmin).toBe(false);
-});
-
-
-test("patch user 404 test 🌞", async () => {
-  let res = await fetch("http://localhost:3000/users/4", {
-    method: "PATCH",
-    body: JSON.stringify({
-      name: "John Doe",
-      email: "john.doe@example.com"
-    })
-  })
-  expect(res.status).toBe(404);
-});
