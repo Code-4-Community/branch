76a5caef8ab4d04950dcf57adb885ccb2d23747f
tsudhakar87 <sudhakar.th@northeastern.edu>
2025-10-30 16:10:28 -0400

    tests for 201 and 400 status cases

--- apps/backend/lambdas/users/test/example.test.ts
+++ apps/backend/lambdas/users/test/example.test.ts
@@ -32,3 +32,48 @@ test("patch user 404 test 🌞", async () => {
   })
   expect(res.status).toBe(404);
 });
+
+test("POST user success case", async () => {
+  let res = await fetch("http://localhost:3000/users/5", {
+    method: "POST",
+    body: JSON.stringify({
+      name: "Jane Branch",
+      email: "jane@branch.com",
+      isAdmin: true
+    })
+  });
+
+  console.log(res)
+  expect(res.status).toBe(201);
+
+  let body = await res.json();
+
+  expect(body.ok).toBe(true);
+  expect(body.body.name).toBe("Jane Branch");
+  expect(body.body.email).toBe("jane@branch.com");
+  expect(body.body.isAdmin).toBe(true);
+});
+
+test("POST user 400 case when invalid userId is sent", async () => {
+  let res = await fetch("http://localhost:3000/users/invalidUserId", {
+    method: "POST",
+    body: JSON.stringify({
+      name: "Invalid User",
+      email: "",
+      isAdmin: false
+    })
+  });
+
+  expect(res.status).toBe(400);
+});
+
+test("POST user 400 case when request sent with missing fields", async () => {
+  let res = await fetch("http://localhost:3000/users/6", {
+    method: "POST",
+    body: JSON.stringify({
+      name: "Invalid User",
+    }) // missing email and admin fields
+  });
+
+  expect(res.status).toBe(400);
+});
