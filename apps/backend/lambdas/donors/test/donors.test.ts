eeab26e6457a1d373e366184813bd3059f9493e5
Rayna-Yu <Alinasharon10@gmail.com>
2025-11-06 22:34:56 -0500

    finish tests

--- apps/backend/lambdas/donors/test/donors.test.ts
+++ apps/backend/lambdas/donors/test/donors.test.ts
@@ -14,42 +14,61 @@ const pool = new Pool({
 const seedSqlPath = path.resolve(__dirname, '../../../db/db_setup.sql');
 const seedSql = fs.readFileSync(seedSqlPath, 'utf8');
 
-beforeEach(async () => {
-  const client = await pool.connect();
-  try {
-    await client.query(seedSql);
-  } finally {
-    client.release();
-  }
+describe("Donor API with data", () => {
+  beforeEach(async () => {
+    const client = await pool.connect();
+    try {
+      await client.query(seedSql);
+    } finally {
+      client.release();
+    }
+  });
+
+  test("health test 🌞", async () => {
+    let res = await fetch("http://localhost:3000/donors/health");
+    expect(res.status).toBe(200);
+  });
+
+  test("Status check for get all donors when donors exist 🌞", async () => {
+    let res = await fetch("http://localhost:3000/donors", { method: "GET" });
+    expect(res.status).toBe(200);
+  });
+
+  test("Content check for get all donors when donors exist 🌞", async () => {
+    let res = await fetch("http://localhost:3000/donors", { method: "GET" });
+    let body = await res.json();
+
+    expect(res.status).toBe(200);
+    expect(Array.isArray(body)).toBe(true);
+    expect(body.length).toBe(3);
+  });
+});
+
+describe("Donor API when DB is empty", () => {
+  beforeEach(async () => {
+    const client = await pool.connect();
+    try {
+      await client.query('TRUNCATE TABLE donors RESTART IDENTITY CASCADE;');
+    } finally {
+      client.release();
+    }
+  });
+
+  test("Status check for get all donors when DB is empty", async () => {
+    let res = await fetch("http://localhost:3000/donors", { method: "GET" });
+    expect(res.status).toBe(200);
+  });
+
+  test("Content check for get all donors when DB is empty", async () => {
+    let res = await fetch("http://localhost:3000/donors", { method: "GET" });
+    let body = await res.json();
+
+    expect(res.status).toBe(200);
+    expect(Array.isArray(body)).toBe(true);
+    expect(body.length).toBe(0);
+  });
 });
 
 afterAll(async () => {
   await pool.end();
-});
-
-test("health test 🌞", async () => {
-  let res = await fetch("http://localhost:3000/donors/health")
-  expect(res.status).toBe(200);
-});
-
-test("Status check for get all donors when donors exists 🌞", async () => {
-  let res = await fetch("http://localhost:3000/donors", {
-    method: "GET"
-  })
-
-  console.log(res)
-
-  expect(res.status).toBe(200);
-});
-
-test("Content check for get all donors when donors exists 🌞", async () => {
-  let res = await fetch("http://localhost:3000/donors", {
-    method: "GET"
-  })
-
-  let body = await res.json();
-
-  expect(res.status).toBe(200);
-  expect(Array.isArray(body)).toBe(true);
-  expect(body.length).toBe(3);
 });
