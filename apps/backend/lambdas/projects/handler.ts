119722d975cedd2734d8c80cc31e4bf8d24b9a6f
Dennis Wang <66754085+denniwang@users.noreply.github.com>
2025-11-13 18:09:24 -0500

    test change

--- apps/backend/lambdas/projects/handler.ts
+++ apps/backend/lambdas/projects/handler.ts
@@ -24,6 +24,11 @@ export const handler = async (event: any): Promise<APIGatewayProxyResult> => {
       const projects = await db.selectFrom("branch.projects").selectAll().execute();
       return json(200, projects);
     }
+
+    // GET /projects
+    if (rawPath === '/test' && method === 'GET') {
+      return json(200, { message: 'Hello, World!' });
+    }
     
     // <<< ROUTES-END    
 
