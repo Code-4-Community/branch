b0c6194f95c69cb34acbcda328a11ffcb3b6badf
Dennis Wang <66754085+denniwang@users.noreply.github.com>
2025-12-04 16:14:14 -0500

    r2

--- apps/backend/lambdas/projects/handler.ts
+++ apps/backend/lambdas/projects/handler.ts
@@ -27,7 +27,6 @@ export const handler = async (event: any): Promise<APIGatewayProxyResult> => {
     // GET /projects
     if (rawPath === '/' && method === 'GET') {
       const projects = await db.selectFrom("branch.projects").selectAll().execute();
-      console.log("dennis");
       return json(200, projects);
     }
     
