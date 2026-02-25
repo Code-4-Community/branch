fadf3bdbb3a8c347494fb19edcc61e1b456b6e13
Mehana Nagarur <nagarur.m@northeastern.edu>
2026-02-25 17:45:48 -0500

    login page done

---
+++ apps/frontend/src/app/components/LoginPage.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import React from 'react';
+import { useState } from 'react';
+import TextInputField from './TextInputField';
+
+export default function LoginPage() {
+    return (
+        <div className="flex flex-col items-center text-center w-80">
+            <h1 className="![font-family:var(--font-heading)] !text-[36px] !font-semibold !mb-6">Login</h1>
+            <h5 className="![font-family:var(--font-body)] !text-[16px] !font-bold !mb-6">BRANCH Accounting Platform</h5>
+            <div className="flex flex-col gap-4 w-full !mb-10">
+                <TextInputField label="Email *" placeholder="Enter email address" errorMessage="Please enter a valid email address"/>
+                <TextInputField label="Password *" placeholder="Enter password" errorMessage="Please enter valid password"/>
+            </div>
+            <button className="![font-family:var(--font-body)] !rounded !bg-core-green !text-core-white w-full !px-4 !py-1.5 !mb-10">Login</button>
+            <h5 className="!text-core-green !font-bold ![font-family:var(--font-body)] !text-[16px]">Forgot password?</h5>
+        </div>
+        
+    );
+}
+
--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,10 +1,20 @@
+'use client';
 import Header from "./components/Header";
+import LoginPage from "./components/LoginPage";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50 font-sans">
       {/* 1. Default Header - Verify this matches Figma */}
       <Header />
+      <div className="flex flex-1 min-h-[calc(100vh-4rem)]">
+        <div className="w-1/2">
+          {/*Tree image */}
+        </div>
+        <div className="w-1/2 flex items-center justify-center">
+          <LoginPage />
+        </div>
+      </div>
     </div>
   );
 }
