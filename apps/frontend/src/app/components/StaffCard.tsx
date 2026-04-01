e9f3c330ba4fbc291176df846a12624a340a2f74
Mehana Nagarur <nagarur.m@northeastern.edu>
2026-04-01 19:01:24 -0400

    changed the staff card to match the new figma designs

--- apps/frontend/src/app/components/StaffCard.tsx
+++ apps/frontend/src/app/components/StaffCard.tsx
@@ -2,32 +2,33 @@
 import React from 'react';
 import Image from 'next/image';
 import { useState } from 'react';
+import { PiUserCircleThin } from "react-icons/pi";
 
 
 interface StaffCardProps {
     image?: string;
     name: string;
-    title: string;
   }
 
 
 export default function StaffCard({
     image,
-    name,
-    title
+    name
   }: StaffCardProps) {
     const [imgError, setImgError] = useState(false);
 
     return (
-        <div className="!w-[90px] h-100 flex flex-col items-center !pt-3 !gap-1 overflow-hidden">
+        <div className="!w-fit !h-fit !flex flex-col items-center !p-3 !gap-1 overflow-hidden !border-1 !border-[var(--color-black-300)] !border-solid">
             {(image && !imgError) ? (
                 <Image src={image} alt="Staff" width={90} height={105} className="object-cover ![aspect-ratio:44/51] w-full" onError={() => setImgError(true)}/>
             ) : (
-                <div className="!w-[90px] ![aspect-ratio:44/51] bg-accent-dark-green"></div>
+                <div data-testid="staff-placeholder" className="!w-fit !h-fit !p-1 bg-[var(--color-primary-300)]">
+                    <PiUserCircleThin size={90} className="text-[var(--color-accent-dark-green)]" />
+                </div>
+                
             )}
             <div className="flex flex-col items-center gap-1 !w-[90px]">
-                <p className="![font-family:var(--font-body)] !text-[length:var(--font-size-body)] !font-normal break-words w-full text-center">{name}</p> 
-                <p className="![font-family:var(--font-family-body)] !text-[length:var(--font-size-callout)] !font-bold break-words w-full text-center">{title}</p>
+                <p className="![font-family:var(--font-body)] !text-[length:var(--font-size-body)] !font-bold break-words w-full text-center">{name}</p> 
             </div>
         </div>
     )
