65ec0e1dabfa1f5760f398028e1ea129a918d72d
tsudhakar87 <sudhakar.th@northeastern.edu>
2026-04-02 18:49:08 -0400

    added amplify.tf

--- infrastructure/aws/variables.tf
+++ infrastructure/aws/variables.tf
@@ -2,3 +2,8 @@ variable "infisical_workspace_id" {
   type    = string
   default = "d1ee8b80-118c-4daf-ae84-31da43261b76"
 }
+
+variable "api_base_url" {
+  type        = string
+  description = "Base URL for the backend API, injected as NEXT_PUBLIC_API_BASE_URL"
+}
