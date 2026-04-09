a42a1661313f6d612fffa3dbc092a7c6040ca229
Rayna-Yu <Alinasharon10@gmail.com>
2026-04-08 22:29:32 -0400

    fix rules to rule

--- infrastructure/aws/lambda.tf
+++ infrastructure/aws/lambda.tf
@@ -35,7 +35,7 @@ resource "aws_s3_bucket_versioning" "lambda_deployments" {
 resource "aws_s3_bucket_server_side_encryption_configuration" "lambda_deployments" {
   bucket = aws_s3_bucket.lambda_deployments.id
 
-  rules {
+  rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
