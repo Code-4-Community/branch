2fac4143b8423feddf413f791ed88834b5acec60
mehanana <mehana.nagarur@gmail.com>
2026-06-01 23:03:02 -0400

    added profile_image to the users db; fixed get/post/patch endpoints to include the new column; wrote a test for this

--- apps/backend/db/db_setup.sql
+++ apps/backend/db/db_setup.sql
@@ -8,7 +8,8 @@ CREATE TABLE users (
     name VARCHAR(100) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
     is_admin BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    profile_image TEXT
 );
 
 CREATE TABLE projects (
